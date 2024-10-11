4f02dfcfe9c2ae3663a8f6c116418dd06b207a85
Tom Mahy <tommahy11@gmail.com>
2024-10-11 13:57:45 +0100

    Updates

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const client_secret = config.client_secret;
 const refresh_token = config.refresh_token;
 const auth_link = 'https://www.strava.com/oauth/token';
 
-console.log(client_id)
-
 async function reAuthorize(){
     try{
         const response = await fetch(auth_link,{
@@ -29,3 +27,26 @@ async function reAuthorize(){
         console.log(e)
     }
 }
+
+// Data needed to retrive or calculate from API data
+// Profile information if possible -> Image and Name
+async function getProfile(){
+    const response = await fetch(``);
+    const profileData = await response.json();
+    console.log(profileData);
+}
+//  Total runs this year, Total distance this year, Total time this year
+async function getTotals(){
+    const response = await fetch(``)
+    
+}
+//  Weekly averages -> runs, distance, time
+async function getWeekly(){
+
+}
+// Top 3 longest runs -> Not sure there is an end point so will have to calculate
+// Top 3 fastest runs -> "---"
+async function getAllActivities(){
+
+}
+
